4c76a86576f4efcf3d95c2a3ffafeb5a3d7cf892
Charles Wang <charlestfeg@gmail.com>
2025-07-18 00:04:36 -0400

    Added extend functionality for menu

--- src/components/side_bar_comp/Sidebar.jsx
+++ src/components/side_bar_comp/Sidebar.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./Sidebar.css"
 import {assets} from '../../assets/assets.js' // uploads the assets objects from the export const in the js file, since it assums assets is a js file, no need of .js tbh
+import { useState } from 'react'
 
 const Sidebar = () => {
-  return (
+    
+    const [extended,setExtended] = useState(false)
+
+return (
     <div className='sidebar'>
         <div className="top">
-            <img className='menu' src={assets.menu_icon} alt="" />
+            <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
             
             <div className="new-chat">
                 <img src={assets.plus_icon} alt="" />
-                <p>New Chat</p>
+                {extended?<p>New Chat</p>:null}
             </div>
 
+            {extended?
             <div className="recent">
                 <p className='recent-title'>Recent</p>
                 <div className="recent-entry">
@@ -20,26 +25,27 @@ const Sidebar = () => {
                     <p>What is react ...</p>
                 </div>
             </div>
+            :null}
         </div>
         
         <div className="bottom">
             <div className="bottom-items recent-entry">
                 <img src={assets.question_icon} alt="" />
-                <p>Help</p>
+                {extended?<p>Help</p>:null}
             </div>
 
             <div className="bottom-items recent-entry">
                 <img src={assets.history_icon} alt="" />
-                <p>Activity</p>
+                {extended?<p>Activity</p>:null}
             </div>
 
             <div className="bottom-items recent-entry">
                 <img src={assets.setting_icon} alt="" />
-                <p>Settings</p>
+                {extended?<p>Settings</p>:null}
             </div>
         </div>
     </div>
-  )
+)
 }
 
 export default Sidebar
